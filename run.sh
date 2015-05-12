522a400a147695180c53fb9e4f2643719f0726ff
viyer-research <vasyncmob@gmail.com>
2015-05-11 21:23:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,17 @@
 
 # first I'll load all my dependencies
 apt-get install python-pandas
-
+#
+# Dependencies for pip in both the programs
+# 1. pip install nltk
+# 2. pip install optparse
+# 3. pip install numpy
+#
+#
 # next I'll make sure that all my programs (written in Python in this example) have the proper permissions
 chmod a+x BatchWC.py
 chmod a+x MedianWPLApp.py
 
 # finally I'll execute my programs, with defaults
-python ./src/BatchWC.py 
-python ./src/MedianWPLApp.py 
+python ./src/BatchWC.py ./wc_input ./wc_output/wc_result.txt
+python ./src/MedianWPLApp.py ./wc_input ./wc_output/med_result.txt
